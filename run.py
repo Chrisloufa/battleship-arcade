ba7ca70f00516f4bb7655637af511e1fc6b07550
Chrisloufa <chris.mcgarry3@gmail.com>
2022-01-20 10:57:47 +0000

    Create game board

--- run.py
+++ run.py
@@ -5,8 +5,17 @@ player_board = [[' '] * 8 for x in range(8)]
 letter_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 """Convert letters to numbers"""
 
-def board():
-    pass
+def print_board(board):
+    """
+    Creates the playing board
+    """
+    print('A B C D E F G H')
+    print('===============')
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
 
 def build_ships():
     pass
@@ -16,3 +25,7 @@ def ship_location():
 
 def sum_hit_ships():
     pass
+
+build_ships()
+turns = 15
+#while turns > 0:
